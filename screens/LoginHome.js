7ce5bdfcc7c9c6908b979710472639eb33430381
Lennon2298 <raghavarora1997@gmail.com>
2019-08-20 21:08:58 +0530

    implementing social auth

--- screens/LoginHome.js
+++ screens/LoginHome.js
@@ -4,41 +4,140 @@ import {
   View,
   Image,
   Text,
+  Linking,
 } from 'react-native';
 
 import SplashScreen from 'react-native-splash-screen';
 import Button from 'apsl-react-native-button';
 import SocialButton from 'rtg-rn-social-buttons';
-//import OAuthManager from 'react-native-oauth';
+import {google, facebook} from 'react-native-simple-auth';
+import axios from 'axios';
+// import OAuthManager from 'react-native-oauth';
 
+// const config =  {
+//   google: {
+//     client_id: '457262287809-hs217glngmohqdd7v03q2c114lb3p00n.apps.googleusercontent.com',
+//     client_secret: 'WkH_C1ZXrUVAIG8Dd2V86TW-'    
+//   },
+//   facebook: {
+//     client_id: 'YOUR_CLIENT_ID',
+//     client_secret: 'YOUR_CLIENT_SECRET'
+//   }
+// }
+// // Create the manager
+// const manager = new OAuthManager('ListedMag')
 
+// // configure the manager
+// manager.configure(config);
+
+const googleConfig = {
+  appId: '457262287809-t8tdnpr7iqrhfhnmpo6glare4ehuf6tj.apps.googleusercontent.com',
+  callback: 'com.listedmagazines:/callback',
+}
+
+const facebookConfig = {
+  appId: '2339211222800047',
+  callback: 'fb2339211222800047://authorize',
+}
 
 export default class LoginHome extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userId: '',
+      RegisterAuth: false,
+    }
   }
   componentDidMount() {
     // do stuff while splash screen is shown
     // After having done stuff (such as async tasks) hide the splash screen
     setTimeout(() => SplashScreen.hide(), 1000);
+    Linking.addEventListener('url', this._handleOpenURL);
+    //this.handleGoogleLogin = this.handleGoogleLogin.bind(this);
   }
 
-  // handleSocialLogin() {
-  //   const config =  {
-  //     google: {
-  //       consumer_key: "457262287809-3hm9kqjftiigd13sm0qb67qqi2pellob.apps.googleusercontent.com",
-  //       consumer_secret: 'Fj0Oe4YAXiRZDORm6Aj5OCcx'
-  //     },
-  //     facebook: {
-  //       client_id: 'YOUR_CLIENT_ID',
-  //       client_secret: 'YOUR_CLIENT_SECRET'
-  //     }
-  //   }
-  //   // Create the manager
-  //   const manager = new OAuthManager('ListedMag')
-  //   // configure the manager
-  //   manager.configure(config);
-  // }
+  _handleOpenURL(event) {
+    console.log(event.url);
+  }
+
+  handleGoogleLogin = () => {
+    console.log("inside google login")
+    google(googleConfig)
+      .then((info) => {
+        console.log(info);
+        //this.setState({userId: info.credentials.access_token});
+        this.setState({userId: info.credentials.access_token});
+        this.handleRegisterRequest();
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+  handleFacebookLogin = () => {
+    console.log("inside facebook login")
+    facebook(facebookConfig)
+      .then((info) => {
+        console.log(info);
+        //this.setState({userId: info.credentials.access_token});
+        this.setState({userId: info.credentials.access_token});
+        this.handleRegisterRequest();
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+  checkRegisterAuth() {
+    if(this.state.RegisterAuth)
+    {
+      //alert("Authenticated");
+      console.log("Authenticated\n" + this.state.email + "\n" +
+       this.state.password + "\n" + this.state.token.key);
+      this.props.navigation.navigate('App');
+    }
+    else{
+      //alert("Not Authenticated\n" + this.state.email + "\n" + this.state.password);
+      //alert()
+      alert("Not Authenticated\n" + this.state.email + "\n" +
+       this.state.password + "\n");
+    }
+  }
+
+  async handleRegisterRequest() {
+    //const endpoint = this.props.create ? 'register' : 'login';
+    const instance = axios.create({
+      baseURL: 'http://192.168.2.209:8000/',
+      timeout: 5000,
+    });
+    //const payload = { email: this.state.email, password1: this.state.password, password2: this.state.password2 }
+    const payload = {access_token: this.state.userId}
+    //if (this.props.create) {
+    //  payload.first_name = this.state.firstName;
+    //  payload.last_name = this.state.lastName;
+    //}
+    //alert("xs");
+    console.log("lolol");
+    await instance
+      .post('rest/auth/facebook/', payload)
+      .then(response => {
+        this.state.token = response.data;
+        AsyncStorage.setItem('user_id', JSON.stringify(response.data));
+        const user = AsyncStorage.getItem('user_id');
+        console.log(user.key);
+        // We set the returned token as the default authorization header
+        axios.defaults.headers.common = {'Authorization': 'Bearer ' + JSON.stringify(this.state.token)};
+        console.log(response);
+        console.log("XD");
+        this.setState({RegisterAuth: true});
+        console.log(axios.defaults.headers.common.Authorization);
+        //this.props.navigation.navigate('Home');
+      })
+      .catch(error => console.log(error));
+
+      //this.checkLoginAuth();
+      this.checkRegisterAuth();
+  }
 
   render() {
     return (
@@ -58,8 +157,9 @@ export default class LoginHome extends Component {
               <View style={styles.loginBody}>
                 <Text style={styles.loginText} allowFontScaling={true} maxFontSizeMultiplier={5}>or</Text>
               </View>
-              <SocialButton type='facebook' text='Continue with Facebook' />
-              <SocialButton type="google" text='Continue with Google' style={styles.loginButton} />
+              <SocialButton type='facebook' text='Continue with Facebook' action={this.handleFacebookLogin} />
+              <SocialButton type="google" text='Continue with Google' style={styles.loginButton} 
+              action={this.handleGoogleLogin} />
             </View>
           </View>
         </View>
